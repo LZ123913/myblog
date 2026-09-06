8e1c6135e6070829ed823e405f932f0aab031868
zllll111 <zhanglong_0712@qq.com>
2026-09-06 17:46:46 +0800

    add github pages deploy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   root: fileURLToPath(new URL('.', import.meta.url)),
   envDir: fileURLToPath(new URL('../', import.meta.url)),
+  base: '/myblog/',
   plugins: [vue()],
   resolve: {
     alias: {
